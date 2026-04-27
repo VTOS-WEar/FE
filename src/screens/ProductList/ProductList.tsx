19394fec8a1873e209b2646eebdb468338ff3353
gtruynvs <vongocgiatruyen@gmail.com>
2026-04-27 13:20:03 +0700

    update structure of uniform

--- src/screens/ProductList/ProductList.tsx
+++ src/screens/ProductList/ProductList.tsx
@@ -558,7 +558,7 @@ export default function ProductList() {
                   </div>
                 </div>
                 
-                <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+                <div className="grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5">
                   {filteredData.allOutfits.map((p) => (
                     <ProductCard key={"all-" + p.id} product={p} />
                   ))}
